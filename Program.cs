72dbff4d1155964b8a1fcee17610950cf5402cb3
Bloark <wildson.luiz@gmail.com>
2022-09-23 22:18:51 -0300

    Implementado testes para avaliar funcionamento das classes.

--- Program.cs
+++ Program.cs
@@ -3,4 +3,13 @@
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
 Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "11111111111", "64");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
 Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("7891011", "Modelo 11", "2222222222", "128");
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
